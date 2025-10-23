a7ca30ffbd89413bf6fc4d2deb5c9620ed371bfd
Tetreur <tetreur@gmail.com>
2025-10-23 15:49:04 +0200

    manpager & char escape

--- hosts/nixos/home/fish/config.fish
+++ hosts/nixos/home/fish/config.fish
@@ -12,7 +12,13 @@ abbr --add h 'history'
 abbr --add z 'zeditor'
 abbr --add  p 'pragma'
 
-alias cat 'bat'
+# Set manpager to use `bat`
+set -gx MANPAGER "sh -c 'awk '\''{ gsub(/\x1B\[[0-9;]*m/, \"\", \$0); gsub(/.\x08/, \"\", \$0); print }'\'' | 'bat' -p -lman'"
+set -gx MANROFFOPT -c
+
+# Colorize help messages
+abbr -a --position anywhere -- --help '--help | 'bat' -plhelp'
+abbr -a --position anywhere -- -h '-h | 'bat' -plhelp'
 
 set -gx PRISMA_QUERY_ENGINE_LIBRARY "$HOME/Projects/prisma_engines/libquery_engine.so.node.gz"
 set -gx PRISMA_SCHEMA_ENGINE_BINARY "$HOME/Projects/prisma_engines/schema-engine.gz"
